a531ed225729e9533beaec776248905d4cb17e69
mjanssen <dev.marnix@gmail.com>
2020-02-22 15:40:16 +0100

    Overhaul of package, implementing cli option

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
+#!/usr/bin/env node
+
+/**
+ * Import vendor modules
+ */
 const program = require('commander');
+
+/**
+ * Import own modules
+ */
+const packageJson = require('../package.json');
+
+/**
+ * Launches the app
+ */
+const run = async () => {
+  /**
+   * Set application name
+   */
+  program.name('gitlogger');
+
+  /**
+   * Set program version
+   */
+  program.version(packageJson.version);
+
+  /**
+   * Set program options
+   */
+  program
+    .option('--current-user', 'use the current GIT user as author', false)
+    .option('--author <author>', 'pass an author', false)
+    .option('--day <YYYY-MM-DD>', 'pass a date of a specific day', false)
+    .option('--since <YYYY-MM-DD>', 'pass a start date', false)
+    .option('--until <YYYY-MM-DD>', 'pass a until date', false)
+    .option('--format <format>', 'formats the git output (default is "%an <%ae> - %s"', '%an <%ae> - %s')
+    .option('--project-path <path>', 'pass a project path, which will be used to search for git directories', __dirname)
+    .option('--skip-directories <string>', 'pass directory names which should be skipped', null)
+    .option('--json', 'give a json response', false);
+
+  /**
+   * Let commander handle process arguments
+   */
+  program.parse(process.argv);
+
+  /**
+   * Run the gitlogs program
+   */
+  require('./program')(program, false, true);
+};
+
+/**
+ * Run the app
+ */
+run();
---
+++ src/program.js
@@ -0,0 +1,152 @@
+const { lstatSync, readdirSync } = require('fs');
+const util = require('util');
+const { join } = require('path');
+const exec = util.promisify(require('child_process').exec);
+const args = require('yargs').argv;
+const dayjs = require('dayjs');
+const dlv = require('dlv');
+const { dim, bold, yellow, magenta } = require('kleur');
+
+let skipDirectories = ['node_modules', 'public', '.git', '_scripts', 'app', 'vendor'];
+let currentUser, author, projectPath, format, day, since, until, _jsonResponse = true;
+const result = {
+  directories: [],
+  entries: [],
+};
+
+function setTime(date, { time = 'morning' }) {
+  return date
+    .set('hour', time === 'morning' ? 0 : 23)
+    .set('minute', time === 'morning' ? 0 : 59)
+    .set('second', time === 'morning' ? 0 : 59);
+}
+
+function isDirectory(source) {
+  return lstatSync(source).isDirectory();
+}
+
+function getDirectoriesInFolder(source) {
+  return readdirSync(source)
+    .map(name => join(source, name))
+    .filter(isDirectory);
+}
+
+function gitTimeFormat(date) {
+  return date.format();
+}
+
+let counts = [0, 0];
+
+async function getGitDirectory(directories, callback = () => { }) {
+  for (let i = 0; i < directories.length; i += 1) {
+    const directory = directories[i];
+    const appendForSplit = '|@|';
+    const f = `${format}${_jsonResponse ? appendForSplit : ''}`;
+    if (directory.match(/\.git/)) {
+      counts[0] += 1;
+      const { stdout, stderr } = await exec(
+        `cd ${directory}; git log --date=local --since="${gitTimeFormat(
+          since
+        )}" --until="${gitTimeFormat(
+          until
+        )}" ${author === false ? '--author=".*"' : `--author=${currentUser ? '$(git config user.name)' : author}`} --pretty=format:"${f}" --no-merges --reverse | cat`)
+
+      const isValid = !stderr && stdout !== '';
+      counts[1] += 1;
+
+      if (_jsonResponse && isValid) {
+        result.directories.push(directory);
+        const regex = new RegExp(`\n|${appendForSplit}`, 'g');
+        result.entries.push(...stdout.split(appendForSplit).map(output => ({ commit: output.replace(regex, ''), directory })).filter(e => e.commit));
+      }
+
+      if (_jsonResponse === false && isValid) {
+        console.log(magenta(bold(dim(directory))));
+        console.log(`${stdout}`);
+        console.log('\n');
+      }
+
+      if (counts[0] === counts[1]) {
+        callback();
+      }
+    }
+
+    if (directory.match(/\.git/) === null) {
+      let directoryIsAllowed = true;
+      skipDirectories.forEach(string => {
+        if (new RegExp(string).test(directory) && directoryIsAllowed === true)
+          directoryIsAllowed = false;
+      });
+
+      if (directoryIsAllowed && isDirectory(directory)) {
+        const lowerDirectories = getDirectoriesInFolder(directory);
+        if (lowerDirectories.length > 0) {
+          getGitDirectory(lowerDirectories, callback);
+        };
+      }
+    }
+  }
+}
+
+async function program(program = {}, jsonResponse = true, cli = false) {
+  _jsonResponse = program.json || jsonResponse;
+
+  const currentDate = dayjs();
+  const startDate = setTime(currentDate, { time: 'morning' });
+  const untilDate = setTime(currentDate, { time: 'night' });
+
+  const authorUndefined = typeof program.author === 'undefined';
+
+  currentUser = program.currentUser || authorUndefined;
+  author = currentUser ? 'current user' : program.author;
+
+  projectPath = program.projectPath;
+  format = program.format;
+  day = program.day;
+  since = program.since;
+  until = program.until;
+
+  since = since !== false ? setTime(dayjs(since), { time: 'morning' }) : startDate;
+  until = until !== false ? setTime(dayjs(until), { time: 'night' }) : untilDate;
+
+  if (day) {
+    since = setTime(dayjs(day), { time: 'morning' });
+    until = setTime(dayjs(day), { time: 'night' });
+  }
+
+  const skipDirectoriesArg = program.skipDirectories;
+
+  skipDirectories = [
+    ...(skipDirectoriesArg ? skipDirectoriesArg.split(',') : []),
+    ...skipDirectories,
+  ];
+
+  const directories = getDirectoriesInFolder(projectPath);
+
+  console.log('\n');
+
+  console.log(
+    yellow(
+      bold(
+        `Getting git logs for ${since.format('DD-MM-YYYY')}${
+        since.format('DD-MM-YYYY') !== until.format('DD-MM-YYYY')
+          ? ` to ${until.format('DD-MM-YYYY')}`
+          : ''
+        }${author === false ? '' : ` from ${author}`}`
+      )
+    )
+  );
+
+  console.log('\n');
+
+  if (_jsonResponse === false) return getGitDirectory(directories);
+
+  return new Promise(resolve => {
+    getGitDirectory(directories, () => {
+      if (cli) console.log(result);
+      return resolve(result);
+    });
+  });
+}
+
+module.exports = program;
