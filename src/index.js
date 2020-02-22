20db6998fbeca67fb02a32a04229de8dcf12648a
mjanssen <dev.marnix@gmail.com>
2020-02-22 15:49:21 +0100

    Updated helper strings

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const run = async () => {
     .option('--format <format>', 'formats the git output (default is "%an <%ae> - %s"', '%an <%ae> - %s')
     .option('--project-path <path>', 'pass a project path, which will be used to search for git directories', __dirname)
     .option('--skip-directories <string>', 'pass directory names which should be skipped', null)
-    .option('--json', 'give a json response', false);
+    .option('--json', 'return a json response', false);
 
   /**
    * Let commander handle process arguments
